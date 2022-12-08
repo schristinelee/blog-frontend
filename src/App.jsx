860d2b1fabf4b4ed5f7ce194dbeda3901fca1434
schristinelee <christinelee1527@gmail.com>
2022-12-08 10:19:01 -0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
+      </header>
+
+      <div id="posts-new">
+        <h1>New post</h1>
+      </div>
+
+      <div id="posts-index">
+        <h1>All posts</h1>
+      </div>
+
+      <footer>
+        <p>Copyright 20XX</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
